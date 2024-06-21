0c596d11a7b711c4630e6458bedd238cd60c9d44
Koren Ma'avari <koren130@gmail.com>
2024-06-21 07:42:06 +0000

    Add files via upload

--- untitled/lib/Koren/emoGuardiansNLP/main.dart
+++ untitled/lib/Koren/emoGuardiansNLP/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'openai_service.dart';
+import 'nlp_service.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'OpenAI Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'בינה מלאכותית',
       home: MyHomePage(),
     );
   }
@@ -24,19 +19,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final OpenAIService openAIService = OpenAIService('YOUR_OPENAI_API_KEY');
+  final NLPService nlpService = NLPService('sk-hackathon-3YXOCwDaUbM5dVzG6y3mT3BlbkFJljboeREJxH78ISOOH5z6');
   String _generatedText = '';
-  String myInput = 'Translate the following English text to French: "Hello, how are you?"';
+  String _userInput = '';
+  bool _isLoading = false;
 
   void _generateText() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     try {
-      final text = await openAIService.emoGuardiansNLP(myInput);
+      final text = await nlpService.emoGuardiansNLP(_userInput);
       setState(() {
         _generatedText = text;
       });
     } catch (e) {
       setState(() {
         _generatedText = 'Error: $e';
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
       });
     }
   }
@@ -45,25 +49,101 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('OpenAI Example'),
+        title: Text('בינה מלאכותית'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: _generateText,
-                child: Text('Generate Text'),
-              ),
-              SizedBox(height: 20),
-              Text(
-                _generatedText,
-                textAlign: TextAlign.center,
-              ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF2E294E),
+              Color(0xFF3E8E9E),
             ],
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(height: 50.0),
+            Center(
+              child: Text(
+                'בינה מלאכותית',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(2.0, 2.0),
+                      blurRadius: 3.0,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                onChanged: (value) {
+                  _userInput = value;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'הכנס מלל',
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFEEB902),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                    horizontal: 50.0,
+                  ),
+                ),
+                onPressed: _generateText,
+                child: Text(
+                  'הפעל בינה מלאכותית',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: _isLoading
+                  ? CircularProgressIndicator()
+                  : Text(
+                _generatedText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'מופעל באמצעות שומרי הנפש\nבינה מלאכותית © 21 ביוני 2024',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12.0,
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+          ],
         ),
       ),
     );
